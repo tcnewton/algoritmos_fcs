44041a9292c634f9310fba6fd8d4262e530aacd2
tcnewton <32221741+tcnewton@users.noreply.github.com>
2017-11-15 21:23:03 -0200

    Initial commit

---
+++ python/leitura de dados/dir_create/importando_texto.py
@@ -0,0 +1,43 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Nov  6 15:01:28 2017
+
+@author: Tales
+"""
+
+import os
+import io
+import requests
+from zipfile import ZipFile
+from tensorflow.python.framework import ops
+ops.reset_default_graph()
+# Download or open data
+data_dir = 'temp'
+data_file = 'text_data.txt'
+if not os.path.exists(data_dir):
+    os.makedirs(data_dir)
+
+if not os.path.isfile(os.path.join(data_dir, data_file)):
+    zip_url = 'http://archive.ics.uci.edu/ml/machine-learning-databases/00228/smsspamcollection.zip'
+    r = requests.get(zip_url)
+    z = ZipFile(io.BytesIO(r.content))
+    file = z.read('SMSSpamCollection')
+    # Format Data
+    text_data = file.decode()
+    text_data = text_data.encode('ascii',errors='ignore')
+    text_data = text_data.decode().split('\n')
+
+    # Save data to text file
+    with open(os.path.join(data_dir, data_file), 'w') as file_conn:
+        for text in text_data:
+            file_conn.write("{}\n".format(text))
+else:
+    # Open data from text file
+    text_data = []
+    with open(os.path.join(data_dir, data_file), 'r') as file_conn:
+        for row in file_conn:
+            text_data.append(row)
+    text_data = text_data[:-1]
+
+text_data = [x.split('\t') for x in text_data if len(x)>=1]
+[text_data_target, text_data_train] = [list(x) for x in zip(*text_data)]
